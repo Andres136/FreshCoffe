caa97654ddd126bc556143ee71282383d841fb56
Elver581 <elveral100@gmail.com>
2026-09-13 11:40:36 -0500

    feat(frontend): mostrar iconos de categorías y datos de productos

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 
+import Resumen from '../components/Resumen'
+import Sidebar from "../components/Sidebar";
+
 export default function Layout() {
   return (
-    <div>
-      layout
+    <div className="md:flex">
+      <Sidebar />
+     <main className="flex-1">
+       <Outlet/>
+     </main>
+      <Outlet/>
       
-      <Outlet />
+      <Resumen />
     </div>
   );
 }
