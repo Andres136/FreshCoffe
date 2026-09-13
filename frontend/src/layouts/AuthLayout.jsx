b98ad5aaa490a366c9cd488714e09efebac26cc0
Elver581 <elveral100@gmail.com>
2026-09-12 21:55:25 -0500

    feat: agregar formulario de registro

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,11 +1,20 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from 'react-router-dom'
 
 export default function AuthLayout() {
   return (
-    <div>
-      AuthLayout
+    <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+      <img 
+       src="/img/logo.svg" 
+       alt="imagen logotipo" 
+       className="max-w-xs"
+      
+
+      />
+
+    <div className='p-19 w-full'>
+      
       <Outlet/>
     </div>
+    </main>
   )
 }
